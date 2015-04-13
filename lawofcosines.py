cb10bff402949c5bca29b592b796f39de89dfc07
virgil0211 <virgil0211@yahoo.com>
2015-04-13 05:44:38 -0500

    Bug fixes
    Imported math module, used "**" operator instead of math.exp command,
    added some "print" lines for individual steps for bug testing (these are
    commented out in the updates).

--- lawofcosines.py
+++ lawofcosines.py
@@ -1,16 +1,17 @@
+import math
 
-a = raw_input("What's the length of a? "),
-b = raw_input("What's the length of b? "),
-c = raw_input("What's the length of c? "),
+a = raw_input("What's the length of a? ")
+b = raw_input("What's the length of b? ")
+c = raw_input("What's the length of c? ")
 
 a = float(a)
 b = float(b)
 c = float(c)
-
-asq = math.exp(a) 
-bsq = math.exp(b)
-csq = math.exp(c)
- 
+#print " %s %s %s ." % (a,b,c)
+asq = a**2 
+bsq = b**2
+csq = c**2
+#print " %s %s %s ." % (asq,bsq,csq)
 
 cosA1 = bsq+csq-asq 
 cosA2 = 2.0*b*c
@@ -19,14 +20,16 @@ cosB2 = 2.0*a*c
 cosC1 = asq+bsq-csq
 cosC2 = 2.0*b*a
 
-cosineA = cosA1//cosA2
-cosineB = cosB1//cosB2
-cosineC = cosC1//cosC2
+#print " %s %s %s %s %s %s ." % (cosA1,cosA2,cosB1,cosB2,cosC1,cosC2)
 
+cosineA = cosA1/cosA2
+cosineB = cosB1/cosB2
+cosineC = cosC1/cosC2
+#print " %s %s %s ." % (cosineA,cosineB,cosineC)
 radangleA = math.acos(cosineA)
 radangleB = math.acos(cosineB)
 radangleC = math.acos(cosineC)
-
+#print " %s %s %s ." % (radangleA,radangleB,radangleC)
 degreeangleA = math.degrees(radangleA)
 degreeangleB = math.degrees(radangleB)
 degreeangleC = math.degrees(radangleC)
